d007bb1ddbc7613ddf923b6bdfe87e0943944c16
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2022-12-19 13:38:03 -0500

    Proyecto terminado

--- src/Conversor/Principal.java
+++ src/Conversor/Principal.java
@@ -4,65 +4,67 @@ import javax.swing.*;
 
 public class Principal {
     public static void main(String[] args) {
-        //Creamos las instacias para poder acceder a los elementos de las clases especificas
+        // Creamos las instancias para poder acceder a los elementos de las clases especificas
         FunctionMonedas monedas = new FunctionMonedas();
         FunctionTemperatura temperatura = new FunctionTemperatura();
 
-        String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversion",
-                "Menu", JOptionPane.QUESTION_MESSAGE, null,
-                new Object[] {"Conversor de moneda", "Conversor de temperatura"},
-                "Seleccion")).toString();
+        // Definimos una variable para controlar el ciclo while
+        boolean continuar = true;
 
-        //Dependiendo de la opcion que elija el usuario en este caso conversor de monedad o conversor de temperatura
+        // Iniciamos el ciclo while
+        while (continuar) {
+            String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión",
+                    "Menú", JOptionPane.QUESTION_MESSAGE, null,
+                    new Object[]{"Conversor de moneda", "Conversor de temperatura", "Salir"},
+                    "Selección")).toString();
 
-        //Usamos la sentencia SWITCH y evaluamos los casos
-        switch (opciones){
-            case  "Conversor de moneda": {
-                //Crear un cuadro de dialogo en el que podamos ingresar un valor
-                String input = JOptionPane.showInputDialog("Ingrese el valor a convertir");
-                Double valorRecibido = Double.parseDouble(input);
-                monedas.ConvertirMonedas(valorRecibido);
-                //boolean acceso = false;
-                int result = JOptionPane.showConfirmDialog(null,
-                        "¿Desea continuar usando el programa?",
-                        "Confirmación", JOptionPane.YES_NO_CANCEL_OPTION);
-                if (result == JOptionPane.YES_OPTION) {
-                    // Llevar al usuario de nuevo al menú principal
+            // Dependiendo de la opción que elija el usuario en este caso conversor de moneda o conversor de temperatura
 
-                } else if (result == JOptionPane.NO_OPTION) {
-                    // Mostrar un mensaje de "Programa Finalizado"
-                    JOptionPane.showMessageDialog(null, "Programa Finalizado");
-                } else {
-                    // Mostrar un mensaje de "Programa Finalizado
-                    JOptionPane.showMessageDialog(null, "Programa Finalizado");
+            // Usamos la sentencia SWITCH y evaluamos los casos
+            switch (opciones) {
+                case  "Conversor de moneda": {
+                    // Crear un cuadro de diálogo en el que podamos ingresar un valor
+                    String input = "";
+                    // Iniciamos un ciclo while para asegurarnos de que el usuario ingresa un valor numérico
+                    while (true) {
+                        try {
+                            input = JOptionPane.showInputDialog("Ingrese el valor a convertir");
+                            Double valorRecibido = Double.parseDouble(input);
+                            monedas.ConvertirMonedas(valorRecibido);
+                            break;
+                        } catch (NumberFormatException e) {
+                            // Si el usuario no ingresa un valor numérico, se muestra un mensaje de error y se vuelve a solicitar el valor
+                            JOptionPane.showMessageDialog(null, "Por favor, ingrese un valor numérico.");
+                        }
+                    }
+                }
+                case "Conversor de temperatura": {
+                    // Recuerda el input que es el valor a convertir
+                    String input2 = "";
+                    // Iniciamos un ciclo while para asegurarnos de que el usuario ingresa un valor numérico
+                    while (true) {
+                        try {
+                            input2 = JOptionPane.showInputDialog("Ingrese el valor a convertir");
+                            Double valorIngresado = Double.parseDouble(input2);
+                            temperatura.ConvertirTemperatura(valorIngresado);
+                            break;
+                        } catch (NumberFormatException e) {
+                            // Si el usuario no ingresa un valor numérico, se muestra un mensaje de error y se vuelve a solicitar el valor
+                            JOptionPane.showMessageDialog(null, "Por favor, ingresa un valor numerico");
+                        }
                     }
                 }
 
-                break;
-            case "Conversor de temperatura":{
-                //Recuerda el input que es el valor a convertir
-                String input2 = JOptionPane.showInputDialog("Ingrese el valor a convertir");
-
-                Double valorIngresado = Double.parseDouble(input2);
-                temperatura.ConvertirTemperatura(valorIngresado);
-                int result = JOptionPane.showConfirmDialog(null, "¿Desea continuar usando el programa?", "Confirmación", JOptionPane.YES_NO_CANCEL_OPTION);
-
-                if (result == JOptionPane.YES_OPTION){
-                    // Llevar al usuario de nuevo al menú principal
+                case "Salir": {
+                    // Mostramos una ventana de confirmación para que el usuario decida si desea salir o no
+                    int opcion = JOptionPane.showConfirmDialog(null, "¿Desea salir del programa?", "Salir", JOptionPane.YES_NO_OPTION);
+                    // Si el usuario elige sí, cambiamos el valor de la variable continuar a false para salir del ciclo while
+                    if (opcion == JOptionPane.YES_OPTION) {
+                        continuar = false;
+                    }
+                    break;
                 }
-                else if (result == JOptionPane.NO_OPTION){
-                    // Mostrar un mensaje de "Programa Finalizado"
-                    JOptionPane.showMessageDialog(null, "Programa Finalizado");
-                }
-                else {
-                    // Mostrar un mensaje de "Programa Finalizado
-                    JOptionPane.showMessageDialog(null, "Programa Finalizado");
-                }
-                break;
-
             }
-
         }
     }
 }
-
